f83f433a971a5117bc4771ddbe591918bfff7ccf
abisi <axel.bisi@epfl.ch>
2023-05-24 17:15:14 +0200

    plotting functions from dj

--- plot_utils/utils.py
+++ plot_utils/utils.py
@@ -5,3 +5,97 @@
 @file: utils.py
 @time: 10.05.2023 10:47
 """
+
+# Imports
+import numpy as np
+from matplotlib import colors
+import matplotlib.colors as mc
+import colorsys
+import scipy.ndimage
+
+
+def remove_top_right_frame(ax):
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    return
+
+def color_to_rgba(color_name):
+    """
+    Converts color name to RGB.
+    :param color_name:
+    :return:
+    """
+
+    return colors.to_rgba(color_name)
+
+def lighten_color(color, amount=0.5):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+    From: https://stackoverflow.com/questions/37765197/darken-or-lighten-a-color-in-matplotlib
+    :param color: Matplotlib color string.
+    :param amount: Number between 0 and 1.
+    :return:
+    """
+
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
+
+
+def adjust_lightness(color, amount=0.5):
+    """
+    Same as lighten_color but adjusts brightness to lighter color if amount>1 or darker if amount<1.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+    From: https://stackoverflow.com/questions/37765197/darken-or-lighten-a-color-in-matplotlib
+    :param color: Matplotlib color string.
+    :param amount: Number between 0 and 1.
+    :return:
+    """
+
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
+
+
+def make_cmap_n_from_color_lite2dark(color, N):
+    """
+    Make ListedColormap from matplotlib color of size N using the lighten_color function.
+    :param color: Matplotlib color string.
+    :param N: Number of colors to have in cmap.
+    :return:
+    """
+    light_factors = np.linspace(0.2, 1, N)
+    cmap = colors.ListedColormap(colors=[lighten_color(color, amount=i) for i in light_factors])
+    return cmap
+
+def halfgaussian_kernel1d(sigma, radius):
+    """
+    Computes a 1-D Half-Gaussian convolution kernel.
+    From: https://stackoverflow.com/questions/71003634/applying-a-half-gaussian-filter-to-binned-time-series-data-in-python
+    """
+    sigma2 = sigma * sigma
+    x = np.arange(0, radius+1)
+    phi_x = np.exp(-0.5 / sigma2 * x ** 2)
+    phi_x = phi_x / phi_x.sum()
+
+    return phi_x
+
+def halfgaussian_filter1d(input, sigma, axis=-1, output=None,
+                      mode="constant", cval=0.0, truncate=4.0):
+    """
+    Convolves a 1-D Half-Gaussian convolution kernel.
+    From: https://stackoverflow.com/questions/71003634/applying-a-half-gaussian-filter-to-binned-time-series-data-in-python
+    """
+    sd = float(sigma)
+    # make the radius of the filter equal to truncate standard deviations
+    lw = int(truncate * sd + 0.5)
+    weights = halfgaussian_kernel1d(sigma, lw)
+    origin = -lw // 2
+    return scipy.ndimage.convolve1d(input, weights, axis, output, mode, cval, origin)
