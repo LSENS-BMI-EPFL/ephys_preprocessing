a2187e90aa083e696b90a4b1ebc760b7d04b30c8
abisi <axel.bisi@epfl.ch>
2025-08-13 11:28:43 +0200

    minor fix: auto get AB or PB name

--- preprocessing/preprocess_spikesort.py
+++ preprocessing/preprocess_spikesort.py
@@ -33,48 +33,55 @@ def main(input_dir, config_file):
     start_time = time.time()
 
     # Get epoch number and run name
-    epoch_name = os.listdir(input_dir)[0]
+    epoch_name = [f for f in os.listdir(input_dir) if '_g' in f][0]
     # Find only directory names with "imec" in it
     n_probes = len([f for f in os.listdir(os.path.join(input_dir, epoch_name)) if 'imec' in f])
     logger.info('Recording using {} probe(s).'.format(n_probes))
 
     # Create output folder
     mouse_name = input_dir.split('\\')[2]
-    session_name = input_dir.split('\\')[-2]
+    if mouse_name.startswith('AB'):
+        session_name = input_dir.split('\\')[-2]
+    elif mouse_name.startswith('PB'):
+        session_name = input_dir.split('\\')[-1]
+    else: # default folder structure
+        session_name = input_dir.split('\\')[-2]
     processed_dir = os.path.join(config['output_path'], mouse_name, session_name, 'Ephys')
     logger.info('Processed data will be saved to {}.'.format(processed_dir))
     pathlib.Path(processed_dir).mkdir(parents=True, exist_ok=True)
 
     # Run CatGT
     logger.info('Starting CatGT.')
-    run_catgt.main(input_dir, processed_dir, config['catgt'])
+    #run_catgt.main(input_dir, processed_dir, config['catgt'])
     logger.info('Finished CatGT in {}.'.format(time.strftime('%H:%M:%S', time.gmtime(time.time()-start_time))))
 
     # Run TPrime a first time to sync whisker artifact times
     logger.info('Starting artifact correction.')
-    run_artifact_correction.main(processed_dir, config)
+    #run_artifact_correction.main(processed_dir, config)
     logger.info('Finished artifact correction in {}.'.format(time.strftime('%H:%M:%S', time.gmtime(time.time()-start_time))))
 
     # Optionally, run OverStrike
     perform_overstrike = False
     if perform_overstrike:
-
         # List of time spans to zero out in recording in secs, relative to start of recording
         if mouse_name == 'AB105':
             timespans_list = [(0,24),(1800,1933),(2389,3161)]
         elif mouse_name == 'AB107':
             timespans_list = [(1247, 1930)]
-        #elif mouse_name == 'AB129':
-        #    timespans_list = [(5580, 1E9)]
+        elif mouse_name == 'AB129':
+            timespans_list = [(5580, 1E9)]
         elif mouse_name == 'AB142':
             timespans_list = [(3474, 3892)]
+        elif mouse_name == 'PB191':
+            timespans_list = [(2350, 2373), (2724, 2778)]
+        elif mouse_name == 'AB149':
+            timespans_list = [(3200, 3524)]
         else:
             timespans_list = [(), ]
 
-
         # Run overstrike on all probes
         logger.info('Starting OverStrike.')
-        run_overstrike.main(processed_dir, config['overstrike'], timespans_list=timespans_list)
+        #run_overstrike.main(processed_dir, config['overstrike'], timespans_list=timespans_list)
         logger.info('Finished OverStrike in {}.'.format(time.strftime('%H:%M:%S', time.gmtime(time.time()-start_time))))
 
     # Run Kilosort
@@ -87,9 +94,9 @@ def main(input_dir, config_file):
     run_bombcell.main(processed_dir, config)
     logger.info('Finished bombcell quality metrics in {}.'.format(time.strftime('%H:%M:%S', time.gmtime(time.time()-start_time))))
 
-    catgt_epoch_name = os.listdir(processed_dir)[0]
+    catgt_epoch_name = [f for f in os.listdir(input_dir) if '_g' in f and 'cat' in f][0]
     exec_time_hhmmss = time.strftime('%H:%M:%S', time.gmtime(time.time()-start_time))
-    logger.success(f'Finished preprocessing in {exec_time_hhmmss} & spike sorting in: {os.path.join(processed_dir, catgt_epoch_name)}. \n You '
+    logger.success(f'Finished preprocessing in {exec_time_hhmmss} & spike sorting in: \n {os.path.join(processed_dir, catgt_epoch_name)}. \n You '
                    f'can now visually check spike sorting results using Phy, then use this path as input to the '
                    f'script preprocessing_sync.py.')
 
@@ -102,7 +109,8 @@ if __name__ == '__main__':
     parser.add_argument('--config', type=str, nargs='?', required=False)
     args = parser.parse_args()
 
-    args.input = r'M:\data\AB142\Recording\AB142_20241128_113227\Ephys' #until \Ephys
+    args.input = r'M:\data\AB105\Recording\AB105_20240314_115206\Ephys' #until \session_id # TODO: PB mice
+    args.input = r'M:\data\PB191\Recording\Ephys\PB191_20241210_110601' #until \session_id # TODO: PB mice
     args.config = r'C:\Users\bisi\ephys_utils\preprocessing\preprocess_config.yaml'
 
     main(args.input, args.config)
